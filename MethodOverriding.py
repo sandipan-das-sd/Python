3eee6c6f71f7f41b523663e5fe471a5108f240e1
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-01-19 12:39:53 +0530

    Code updated

--- MethodOverriding.py
+++ MethodOverriding.py
@@ -5,13 +5,19 @@ class Shape:
 
     def area(self):
         return self.x * self.y
+
 class Circle(Shape):
     def __init__(self, radius):
-        self.radius=radius
-        super().__init__(radius,radius)
+        self.radius = radius
+        super().__init__(radius, radius)
+
+    def area(self):
+        return 3.14 * super().area()
+
 # Create an instance of the Shape class
 rec = Shape(3, 5)
-C=Circle(5)
+C = Circle(5)
+
 # Call the area method by adding parentheses to get the result
-print(rec.area())
-print(C.area())
+print("Rectangle Area:", rec.area())
+print("Circle Area:", C.area())
