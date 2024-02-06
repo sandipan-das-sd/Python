205c6c2f2c11a52bec9f82231240f5dfcfb2b528
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-02-06 23:16:41 +0530

    question 9 added

---
+++ Practical Exams/Question 9.py
@@ -0,0 +1,9 @@
+# 8.	Write a Python program to Sort a List of elements programmatically (ascending).
+# Define a list of elements
+elements = [5, 2, 7, 3, 9, 1, 4, 6, 8]
+
+# Sort the list in ascending order
+sorted_elements = sorted(elements)
+
+# Print the sorted list
+print("Sorted elements in ascending order:", sorted_elements)
