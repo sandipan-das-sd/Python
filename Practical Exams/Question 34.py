8ec230f70b93a2a528c97a58f8e8c668ed82da08
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-02-06 23:51:54 +0530

    question 34 updated

--- Practical Exams/Question 34.py
+++ Practical Exams/Question 34.py
@@ -1,26 +1,34 @@
 # 34.	Write a program to create two lists as the square and cube of the elements of the given list.
 n = int(input("How many numbers do you want to insert: "))
 my_list = []
-
+square_list=[]
+cube_list=[]
 print("Enter the elements:")
 for i in range(n):
     ele = int(input())
     my_list.append(ele)
+for i in range(len(my_list)):
+    square_list.append(my_list[i]**2)
+print("The square list is:-",square_list)
 
-def square(lst):
-    square_lst = []
-    for i in range(len(lst)):
-        square_lst.append(lst[i]**2)
-    return square_lst
+for i in range(len(my_list)):
+    cube_list.append(my_list[i]**3)
+print("The cube list is:-",cube_list)
 
-def cube(lst):
-    cube_lst = []
-    for i in range(len(lst)):
-        cube_lst.append(lst[i]**3)
-    return cube_lst
+# def square(lst):
+#     square_lst = []
+#     for i in range(len(lst)):
+#         square_lst.append(lst[i]**2)
+#     return square_lst
 
-square_list = square(my_list)
-cube_list = cube(my_list)
-print("The original list is:", my_list)
-print("List of Squares:", square_list)
-print("List of Cubes:", cube_list)
+# def cube(lst):
+#     cube_lst = []
+#     for i in range(len(lst)):
+#         cube_lst.append(lst[i]**3)
+#     return cube_lst
+
+# square_list = square(my_list)
+# cube_list = cube(my_list)
+# print("The original list is:", my_list)
+# print("List of Squares:", square_list)
+# print("List of Cubes:", cube_list)
