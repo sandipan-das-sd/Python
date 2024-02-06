0175a23d44c3d2a25655011ed011a99fd748d169
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-02-06 22:43:57 +0530

    updated pascal traingle

--- Practical Exams/Question 33_pascal.py
+++ Practical Exams/Question 33_pascal.py
@@ -1,18 +1,10 @@
-# Print Pascal's Triangle in Python
+
 from math import factorial
-
-# input n
-n = int(input("Enter the number of rows:-\n"))
+n=int(input("Enter the number of rows:-\n"))
 for i in range(n):
-	for j in range(n-i+1):
+    for j in range(n-i+1):
+        print(end=" ")
+    for j in range(i+1):
+        print(factorial(i)//(factorial(j)*factorial(i-j)),end=" ")
 
-		# for left spacing
-		print(end=" ")
-
-	for j in range(i+1):
-
-		# nCr = n!/((n-r)!*r!)
-		print(factorial(i)//(factorial(j)*factorial(i-j)), end=" ")
-
-	# for new line
-	print()
+    print()
