ea19d5dfec794f0703f22284fca2bee63c8c9484
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-02-06 23:55:51 +0530

    question 36 updated

--- Practical Exams/Question 36.py
+++ Practical Exams/Question 36.py
@@ -1,25 +1,19 @@
-# 36.  Write a program to get all the automorphic number within a given range. 
 start = int(input("From where you want to check the automorphic number: "))
 end = int(input("Up to which you want to check the automorphic number: "))
 
-def is_automorphic(num):
-    square = num ** 2
-    str_num = str(num)
-    str_square = str(square)
-    return str_square.endswith(str_num)
-
-# Create a list to store automorphic numbers
-automorphic_number_list = []
+# Iterate through the numbers in the given range
+automorphic_numbers = []
 
 for num in range(start, end + 1):
-    if is_automorphic(num):
-        automorphic_number_list.append(num)
+    square = num ** 2
+    if str(square).endswith(str(num)):
+        automorphic_numbers.append(num)
 
+print("The automorphic number list is:", automorphic_numbers)
 
-print("The automorphic number list is:", automorphic_number_list)
-
+# Check if a specific number is automorphic
 num_to_check = int(input("Enter a number to check if it's automorphic: "))
-if num_to_check in automorphic_number_list:
+if num_to_check in automorphic_numbers:
     print("The number", num_to_check, "is automorphic.")
 else:
     print("The number", num_to_check, "is not automorphic.")
