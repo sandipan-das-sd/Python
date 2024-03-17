11101a0cd099a11405f8aa06b13d13946ec1a765
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-03-17 20:51:45 +0530

    constructor ADDED

--- Oopps/constructor.py
+++ Oopps/constructor.py
@@ -1,55 +1,70 @@
+
+### First Program:
+# ```python
 class Student:
     name="Sandipan Das"
-    def __init__(self) :
+    def __init__(self):
         print(self)
         print("Adding new student in the database... ")
    
-
-
-s1=Student()
+s1 = Student()
 print(s1.name)
 print(s1)
-#s1 object and self are same thing self is pointing to the object s1
-#self alwyas pointing to the first parameter 
-'''
-<__main__.Student object at 0x000001F3589EDBD0>
-Adding new student in the database...
-Sandipan Das
-<__main__.Student object at 0x000001F3589EDBD0>  
-PS F:\coding\Python>
-'''
+# ```
+# - In this program, the class `Student` has a class attribute `name` set to `"Sandipan Das"`.
+# - It defines an `__init__` method without any parameters.
+# - When an instance of `Student` (`s1`) is created, it prints the instance's address and a message.
+# - It then prints the `name` attribute of `s1`, which is `"Sandipan Das"`, and the instance's address.
 
-#second programe
+### Second Program:
+# ```python
 class Student:
-    
-    def __init__(self,fullname) :
-        # print(self)
-        self.name=fullname
-
+    def __init__(self, fullname):
+        self.name = fullname
         print("Adding new student in the database... ")
    
-
-
-s1=Student("Ranita")
+s1 = Student("Ranita")
 print(s1.name)
-s2=Student("arjun")
+s2 = Student("arjun")
 print(s2.name)
+# ```
+# - This version of `Student` takes a `fullname` parameter in its `__init__` method and assigns it to the instance's `name` attribute.
+# - When instances `s1` and `s2` are created, it prints a message indicating the addition of a new student along with the provided name.
 
-#Third programe
+### Third Program:
+# ```python
 class Student:
-    
-    def __init__(self,name,marks) :
-        # print(self)
-        self.name=name
-        self.marks=marks
-
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
         print("Adding new student in the database... ")
    
-
-
-s1=Student("Ranita",98)
+s1 = Student("Ranita", 98)
 print(s1.name)
 print(s1.marks)
-s2=Student("arjun",99)
+s2 = Student("arjun", 99)
 print(s2.name)
 print(s2.marks)
+# ```
+# - Similar to the second program, but this time it takes both `name` and `marks` parameters.
+# - When instances `s1` and `s2` are created, it prints a message indicating the addition of a new student along with the provided name and marks.
+
+### Fourth Program:
+# ```python
+class Student:
+    def __init__(self):
+        pass
+     
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+        print("Adding new student in the database... ")
+   
+s1 = Student("Ranita", 98)
+print(s1.name)
+# ```
+# - This version tries to define two `__init__` methods, which is not allowed in Python.
+# - Python doesn't support method overloading like some other languages.
+# - Only the last `__init__` method will be considered, so the `__init__` method without parameters will not be accessible.
+
+# In Python, if you want different behavior based on the number or type of arguments, you can use default arguments or conditional statements within a single `__init__` method.
