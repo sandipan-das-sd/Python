5cd406e33b0b7baa944ef6b8ddc974bc338d2398
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-03-23 11:56:43 +0530

    quick sort added

--- DSA WITH PYTHON/quickSort.py
+++ DSA WITH PYTHON/quickSort.py
@@ -1,1 +1,48 @@
-#Quick_sort
+def swap(a, b, arr):
+    '''
+    Function to swap elements at indices a and b in the array arr.
+    '''
+    if a != b:  # If the indices are different
+        tmp = arr[a]  # Store the value at index a temporarily
+        arr[a] = arr[b]  # Assign the value at index b to index a
+        arr[b] = tmp  # Assign the temporary value to index b
+
+
+def partition(elements):
+    '''
+    Function to partition the elements around a pivot.
+    '''
+    pivot_index = 0  # Index of the pivot element
+    pivot = elements[pivot_index]  # Pivot element
+    start = pivot_index + 1  # Starting index for comparison
+    end = len(elements) - 1  # Ending index for comparison
+    while start < end:  # Loop until start and end indices meet
+
+        # Move the start index to the right until we find an element greater than pivot
+        while elements[start] <= pivot:
+            start += 1
+
+        # Move the end index to the left until we find an element less than or equal to pivot
+        while elements[end] > pivot:
+            end = end - 1
+
+        # If start index is still less than end index, swap elements at start and end indices
+        if start < end:
+            swap(start, end, elements)
+
+    # Swap pivot element with element at end index
+    swap(pivot_index, end, elements)
+
+
+def quick_sort(elements):
+    '''
+    Function to perform quick sort on the list of elements.
+    '''
+    partition(elements)  # Call partition function
+
+
+if __name__ == '__main__':
+    # Test case
+    elements = [11, 9, 29, 7, 2, 15, 28]
+    quick_sort(elements)  # Call quick_sort function
+    print(elements)  # Print sorted elements
