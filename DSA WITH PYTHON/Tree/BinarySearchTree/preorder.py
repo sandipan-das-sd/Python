68f3e6cad29e7fe9635d99046ffd78f9a4a4e2fc
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-03-24 10:38:35 +0530

    Preorder traversal modified

--- DSA WITH PYTHON/Tree/BinarySearchTree/preorder.py
+++ DSA WITH PYTHON/Tree/BinarySearchTree/preorder.py
@@ -1,44 +1,45 @@
 class BinarySearchTreeNode:
-    def __init__(self,data):
-        self.data=data
-        self.right=None
-        self.left=None
+    def __init__(self, data):
+        self.data = data
+        self.right = None
+        self.left = None
 
-def add_child(self,data):
-    if data==self.data:
-        return
-    if data<self.data:
-        #add the data in the left subtree
-           if self.left: # you are not leaf node
+    def add_child(self, data):
+        if data == self.data:
+            return
+        if data < self.data:
+            # add the data in the left subtree
+            if self.left:
+                # you are not a leaf node
                 self.left.add_child(data)
-           else:
-                self.left=BinarySearchTreeNode(data)
-    else:
-        #add data to right subtree
-        self.right=BinarySearchTreeNode(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            # add data to right subtree
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
 
-def in_order_traversal(self):
-#visit left subtreee
-     elements=[]
-     if self.left:
-          elements+=self.left.in_order_traversal()
-        #visit base node
-          elements.append(self.data)
-    #visit right subtreee
-          elements+=self.right.in_order_traversal()
-          return elements
-def bulid_tree(elements):
-     root=BinarySearchTreeNode(elements[0])
-     for i in range(1,len(elements)):
-          root.add_child(elements[i])
-          return root
-if __name__=='__main__':
-     numbers=[1,2,3,4,5,6]
-numbers_tree=bulid_tree(numbers)
-print(numbers_tree.in_order_traversal())
-     
+    def in_order_traversal(self):
+        # visit left subtree
+        elements = []
+        if self.left:
+            elements += self.left.in_order_traversal()
+        # visit base node
+        elements.append(self.data)
+        # visit right subtree
+        if self.right:
+            elements += self.right.in_order_traversal()
+        return elements
 
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+    for i in range(1, len(elements)):
+        root.add_child(elements[i])
+    return root
 
-   
-
-        
+if __name__ == '__main__':
+    numbers = [1, 2, 3, 4, 5, 6]
+    numbers_tree = build_tree(numbers)
+    print(numbers_tree.in_order_traversal())
