e05a05976739628101968cf65522d13fba690af9
Sandipan Das <sandipanpyqplatform@gmail.com>
2024-03-24 10:59:57 +0530

    2nd method of pre order

--- DSA WITH PYTHON/Tree/BinarySearchTree/preorder.py
+++ DSA WITH PYTHON/Tree/BinarySearchTree/preorder.py
@@ -43,3 +43,47 @@ if __name__ == '__main__':
     numbers = [5,8,1,6,10,2,3,58,9,7,12]
     numbers_tree = build_tree(numbers)
     print(numbers_tree.in_order_traversal())
+
+
+#Method 2
+'''
+# Python program for preorder traversals
+
+# Structure of a Binary Tree Node
+class Node:
+    def __init__(self, v):
+        self.data = v
+        self.left = None
+        self.right = None
+
+# Function to print preorder traversal
+def printPreorder(node):
+    if node is None:
+        return
+
+    # Deal with the node
+    print(node.data, end=' ')
+
+    # Recur on left subtree
+    printPreorder(node.left)
+
+    # Recur on right subtree
+    printPreorder(node.right)
+
+
+# Driver code
+if __name__ == '__main__':
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(3)
+    root.left.left = Node(4)
+    root.left.right = Node(5)
+    root.right.right = Node(6)
+
+    # Function call
+    print("Preorder traversal of binary tree is:")
+    printPreorder(root)
+
+
+
+'''
